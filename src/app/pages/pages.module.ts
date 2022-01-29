5f79a4eac143cc185ac92f96f970fc25973eaaf7
Luis Ferreira <luis19nn@gmail.com>
2022-01-29 15:00:47 -0300

    Routing and Homepage added

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//Module Routing
+import { RoutingModule } from './routing.module';
+
+//Pages
+import { HomeComponent } from './home/home.component';
+
+//Components
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, RoutingModule],
+})
+export class PagesModule {}
